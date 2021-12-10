83a1a72d71abead1f161e9c6c3fc87b6c78db174
Josh Collinsworth <josh.collinsworth@shopify.com>
2021-12-10 15:21:08 -0600

    Update icons and images

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   pwa: {
     name: "Liquid Measurement Converter",
-    themeColor: "#ffd100"
+    themeColor: "#ffd100",
+    icons: {
+      favicon32: 'img/icons/ios/32.png',
+      favicon16: 'img/icons/ios/16.png',
+      appleTouchIcon: 'img/icons/ios/152.png',
+      maskIcon: 'img/icons/maskable.svg',
+      msTileImage: 'img/icons/ios/144.png'
+    }
   }
 };
